df833a40dfc7b78258740317d8137f086e524641
Sabine Mueller <sabine.b.mueller@itwm.fraunhofer.de>
2021-03-17 15:49:58 +0100

    added timeouts and default values such that ci tests can check whether the scripts work.

--- install.sh
+++ install.sh
@@ -4,18 +4,22 @@
 echo "entering dart directory..."
 cd dart
 
-echo -n "Is your system based on x86 [(y)es,(n)o]? "
-read setting
+echo -n "Is your system based on x86 [(y)es,(n)o]? (Installation will default to x86) "
+read -t 5 setting
 
 
 if [ "$setting" == "y" ]; then
     echo "Installing for x86 machines..."
     #echo "Downloading files..."
     tar xvf dart_x86_64.tar.gz
-else
+elif [ "$setting" == "n" ]; then
     echo "Installing for ARM machines..."
     #echo "Downloading files..."
     tar xvf dart_arm.tar.gz
+else
+    echo 
+    echo " Timeout: Default installation for x86 machines..."
+    tar xvf dart_x86_64.tar.gz
 fi
 
 echo "leaving dart directory..."
--- prepare_conda.sh
+++ prepare_conda.sh
@@ -3,14 +3,18 @@
 conda deactivate
 conda env remove -n fed_dart
 
-echo -n "Do you want to install tensorflow [(y)es,(n)o]? "
-read tf
+echo -n "Do you want to install tensorflow [(y)es,(n)o]? (Default: without tensorflow) "
+read -t 5 tf
 
 # create new environment and install w/wo tensorflow
 if [ "$tf" == "y" ]; then
     echo "installing with tensorflow..."
-    conda create -n fed_dart python=3.7.3 tensorflow=2.1 pip numpy requests dill 
-else
+    conda create -y -n fed_dart python=3.7.3 tensorflow=2.1 pip numpy requests dill 
+elif [ "$tf" == "n" ]; then
     echo "installing without tensorflow..."
-    conda create -n fed_dart python=3.9 pip numpy requests dill
+    conda create -y -n fed_dart python=3.9 pip numpy requests dill
+else
+    echo 
+    echo " Timeout: installing without tensorflow"
+    conda create -y -n fed_dart python=3.9 pip numpy requests dill
 fi
