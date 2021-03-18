cb743c7539017f264a9b493032f0daf074d296a2
Sabine Mueller <sabine.b.mueller@itwm.fraunhofer.de>
2021-03-18 11:11:01 +0100

    no idea what is changed wrt to the last commit - probably an empty line

--- prepare_conda.sh
+++ prepare_conda.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # remove old conda environment
+ENVNAME=fed_dart_env
+
+# switch to base environment
 conda deactivate
-conda env remove -n fed_dart
+# remove old conda env
+conda env remove -n ${ENVNAME}
 
 echo -n "Do you want to install tensorflow [(y)es,(n)o]? (Default: without tensorflow) "
 read -t 5 tf
@@ -9,12 +13,12 @@ read -t 5 tf
 # create new environment and install w/wo tensorflow
 if [ "$tf" == "y" ]; then
     echo "installing with tensorflow..."
-    conda create -y -n fed_dart python=3.7.3 tensorflow=2.1 pip numpy requests dill 
+    conda create -y -n ${ENVNAME} python=3.7.3 tensorflow=2.1 pip numpy requests dill 
 elif [ "$tf" == "n" ]; then
     echo "installing without tensorflow..."
-    conda create -y -n fed_dart python=3.9 pip numpy requests dill
+    conda create -y -n ${ENVNAME} python=3.9 pip numpy requests dill
 else
     echo 
     echo " Timeout: installing without tensorflow"
-    conda create -y -n fed_dart python=3.9 pip numpy requests dill
+    conda create -y -n ${ENVNAME} python=3.9 pip numpy requests dill
 fi
