c8fed37c355ec5d5cb8e67d975cd8dde63085776
Sabine Mueller <sabine.b.mueller@itwm.fraunhofer.de>
2021-03-22 12:27:20 +0100

    tests...

--- prepare_conda.sh
+++ prepare_conda.sh
@@ -9,7 +9,7 @@ conda deactivate
 # remove old conda env
 conda env remove -n ${ENVNAME}
 
-echo -n "Do you want to install tensorflow [(y)es,(n)o]? (Default: with tensorflow) "
+echo -n "Do you want to install tensorflow [(y)es,(n)o]? (Default: without tensorflow) "
 read -t 5 tf
 
 # create new environment and install w/wo tensorflow
@@ -21,8 +21,8 @@ elif [ "$tf" == "n" ]; then
     conda create -y -n ${ENVNAME} python=3.9 pip numpy requests dill
 else
     echo 
-    echo " Timeout: installing with tensorflow"
-    conda create -y -n ${ENVNAME} python=3.9 pip numpy requests dill tensorflow=2.4
+    echo " Timeout: installing without tensorflow"
+    conda create -y -n ${ENVNAME} python=3.9 pip numpy requests dill 
 fi
 
 echo "Install fed-dart package..."
